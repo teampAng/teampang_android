57d4452bc36b19d9cb493cc463c989d4b9f3ecf8
tjcodus98 <alice_98@naver.com>
2021-02-02 19:27:27 +0900

    [docs] 작업 시작 전 주석 한줄 추가 body: tabbar 부분이 어디인지 확인하기 위해 주석 추가

--- app/src/main/java/com/alice/teampang/MainActivity.kt
+++ app/src/main/java/com/alice/teampang/MainActivity.kt
@@ -3,6 +3,8 @@ package com.alice.teampang
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// custom tab bar main activity
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
