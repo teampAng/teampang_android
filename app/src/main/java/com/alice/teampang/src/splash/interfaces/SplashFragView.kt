714d77e35078b42161cf0e891d7bab4e8d4308df
tjcodus98 <alice_98@naver.com>
2021-02-24 02:20:19 +0900

    [api] 프로필 조회 GET 스플래시 화면에 엮음

---
+++ app/src/main/java/com/alice/teampang/src/splash/interfaces/SplashFragView.kt
@@ -0,0 +1,10 @@
+package com.alice.teampang.src.splash.interfaces
+
+import com.alice.teampang.src.splash.model.*
+
+interface SplashFragView {
+
+    fun getProfileSuccess(getProfileResponse: GetProfileResponse)
+
+    fun getProfileFailure(message: Throwable?)
+}
