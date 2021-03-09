a81081c04b64822c8cb1bfc2885da8c61e2aa886
tjcodus98 <alice_98@naver.com>
2021-03-09 14:43:25 +0900

    [api] 개인 일정 생성 POST 개인일정 추가 뷰에 엮음

--- app/src/main/java/com/alice/teampang/src/my_schedule/MyScheduleService.kt
+++ app/src/main/java/com/alice/teampang/src/my_schedule/MyScheduleService.kt
@@ -2,14 +2,9 @@ package com.alice.teampang.src.my_schedule
 
 import com.alice.teampang.src.GlobalApplication.Companion.getRetrofit
 import com.alice.teampang.src.error.ErrorUtils
-import com.alice.teampang.src.login.interfaces.LoginFragView
-import com.alice.teampang.src.login.interfaces.LoginRetrofitInterface
-import com.alice.teampang.src.login.model.*
+import com.alice.teampang.src.my_schedule.model.*
 import com.alice.teampang.src.my_schedule.interfaces.MyScheduleFragView
 import com.alice.teampang.src.my_schedule.interfaces.MyScheduleRetrofitInterface
-import com.alice.teampang.src.my_schedule.model.MyScheduleResponse
-import com.alice.teampang.src.splash.interfaces.SplashRetrofitInterface
-import com.alice.teampang.src.splash.model.GetProfileResponse
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
@@ -43,32 +38,6 @@ class MyScheduleService(myScheduleFragView: MyScheduleFragView) {
             }
         })
     }
-
-//    fun postKakaoToken(kakaoTokenBody: KakaoTokenBody) {
-//        val myScheduleRetrofitInterface: MyScheduleRetrofitInterface = getRetrofit()!!.create(
-//            MyScheduleRetrofitInterface::class.java
-//        )
-//        myScheduleRetrofitInterface.postKakoToken(kakaoTokenBody).enqueue(object :
-//            Callback<KakaoTokenResponse?> {
-//            override fun onResponse(
-//                call: Call<KakaoTokenResponse?>,
-//                response: Response<KakaoTokenResponse?>
-//            ) {
-//                val kakaoTokenResponse: KakaoTokenResponse? = response.body()
-//                val error: ResponseBody? = response.errorBody()
-//                if (kakaoTokenResponse == null) {
-//                    if (error != null) mMyScheduleFragView.kakaoTokenError(ErrorUtils.parseError(error))
-//                    else mMyScheduleFragView.kakaoTokenFailure(null)
-//                    return
-//                }
-//                mMyScheduleFragView.kakaoTokenSuccess(kakaoTokenResponse)
-//            }
-//
-//            override fun onFailure(call: Call<KakaoTokenResponse?>, t: Throwable) {
-//                mMyScheduleFragView.kakaoTokenFailure(t)
-//            }
-//        })
-//    }
 }
 
 
--- app/src/main/java/com/alice/teampang/src/my_schedule/edit/MyScheduleEditAdapter.kt
+++ app/src/main/java/com/alice/teampang/src/my_schedule/edit/MyScheduleEditAdapter.kt
@@ -1,4 +1,4 @@
-package com.alice.teampang.src.my_schedule
+package com.alice.teampang.src.my_schedule.edit
 
 import android.content.Context
 import android.view.LayoutInflater
@@ -7,30 +7,29 @@ import androidx.recyclerview.widget.RecyclerView
 import com.alice.teampang.databinding.MyScheduleEditListBinding
 import com.alice.teampang.src.my_schedule.model.Times
 
-class MyScheduleEditAdapter (private val context : Context) : RecyclerView.Adapter<MyScheduleEditAdapter.ProfileVH>() {
+class MyScheduleEditAdapter (private val context : Context) : RecyclerView.Adapter<MyScheduleEditAdapter.ViewHolder>() {
 
     var data = arrayListOf<Times>()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProfileVH {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = MyScheduleEditListBinding.inflate(
             LayoutInflater.from(context), parent, false
         )
 
-        return ProfileVH(binding)
+        return ViewHolder(binding)
     }
 
     override fun getItemCount(): Int = data.size
 
-    override fun onBindViewHolder(holder: ProfileVH, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.onBind(data[position])
         holder.binding.btnDelete.setOnClickListener {
-            //개인일정 삭제 api 쏘기
             data.removeAt(position)
-            this.notifyDataSetChanged()
+            this.notifyItemRemoved(position)
         }
     }
 
-    class ProfileVH(val binding: MyScheduleEditListBinding) : RecyclerView.ViewHolder(binding.root) {
+    class ViewHolder(val binding: MyScheduleEditListBinding) : RecyclerView.ViewHolder(binding.root) {
         fun onBind(data: Times) {
             binding.times = data
         }
